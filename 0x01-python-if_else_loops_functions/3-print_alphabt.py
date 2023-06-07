49c0587772e98301d430a239dc31a41f52806033
medyasine <yasinmohamed89@gmail.com>
2023-06-07 09:13:10 +0000

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# 3-print_alphabt.py
+
+"""prints the ASCII alphabet, in lowercase, not followed by a new line."""
 for letter in range(97, 123):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
         print("{}".format(chr(letter)), end="")
